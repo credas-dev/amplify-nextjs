1c30eb2a5c652aa6c461f5636a6e166368816aa8
Dev <jmorris@credas.us>
2022-02-01 15:57:25 -0600

    updated api

---
+++ next-authentication/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nextauthentication9bba052a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- next-authentication/pages/api/hello.js
+++ next-authentication/pages/api/hello.js
@@ -1,4 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Amplify, { withSSRContext } from 'aws-amplify'
+import config from "../../src/aws-exports.js"
+
+// Amplify SSR configuration needs to be enabled within each API route
+Amplify.configure({ ...config, ssr: true })
+
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
